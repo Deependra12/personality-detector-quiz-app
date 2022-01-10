434faf44472d3fcc4305dcdf8cec55d0a62f9135
deependra12 <dg02432018@student.ku.edu.np>
2022-01-10 19:57:31 +0545

    button and function

--- basic/lib/main.dart
+++ basic/lib/main.dart
@@ -5,13 +5,40 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('answer clicked');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home:Scaffold(
-        appBar: AppBar(title: const Text('my first app'),),
-        body:const Text ('welcome '),
-        )
-    );
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('my first app'),
+      ),
+      body: Column(
+        // ignore: prefer_const_literals_to_create_immutables
+        children: [
+          const Text('Question'),
+          RaisedButton(
+              onPressed: answerQuestion,
+              child: const Text(
+                'Answer 1',
+              )),
+          RaisedButton(
+              onPressed: () {
+                print('answer 2 chosen');
+              },
+              child: const Text(
+                'Answer 2',
+              )),
+          RaisedButton(
+              onPressed: () => print('answer 3 chosen'),
+              child: const Text(
+                'Answer 3',
+              )),
+        ],
+      ),
+    ));
   }
 }
